0df075650e25d848dac930ddb36fb0b30fc90c6e
“abhishek” <abhishananda@gmail.com>
2022-03-08 19:03:41 +0530

    Second Commit

---
+++ JavaLearnBasics/src/Input_Output_Loops_And_Conditionals_In_Java/Main.java
@@ -0,0 +1,17 @@
+package Input_Output_Loops_And_Conditionals_In_Java;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("your input");
+		//int n = scn.nextInt();
+		String s = scn.next();
+		System.out.println(s);
+		s = scn.next();
+		System.out.println(s);
+	}
+
+}
